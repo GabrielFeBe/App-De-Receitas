9855aeefc5e6a726201653556a9770e23c130558
roger-mont <roger_galvin@hotmail.com>
2023-04-06 14:51:20 -0400

    requisitos 57 ao 59 Ok

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function Profile() {
+  const [user] = useLocalStorage('user', 'email@email.com');
   return (
     <div>
       <Header />
-      Profile
+      <h1 data-testid="profile-email">{user.email}</h1>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+
+      </button>
+
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+
+      </button>
       <Footer />
     </div>
   );
