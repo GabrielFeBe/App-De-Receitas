91ba842f5c7200aef333f14eca0b40f312440b99
roger-mont <roger_galvin@hotmail.com>
2023-04-11 15:46:02 -0400

    requisito 49 feito

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import clipboardCopy from 'clipboard-copy';
+import { useHistory } from 'react-router-dom';
 import shareButton from '../images/shareIcon.svg';
 import Header from '../components/Header';
 import useLocalStorage from '../hooks/useLocalStorage';
@@ -7,6 +8,7 @@ import useLocalStorage from '../hooks/useLocalStorage';
 function DoneRecipes() {
   const [doneRecipes] = useLocalStorage('doneRecipes', [{}]);
   const [copied, setCopied] = useState(false);
+  const history = useHistory();
   return (
     <div>
       <Header />
@@ -18,13 +20,21 @@ function DoneRecipes() {
         recipe.type === 'meal'
           ? (
             <>
-              <img
-                style={ { width: '260px' } }
-                alt="icone do botao"
-                key={ index }
-                src={ recipe.image }
-                data-testid={ `${index}-horizontal-image` }
-              />
+              <button
+                type="button"
+                onClick={
+                  () => history.push(`/meals/${recipe.id}`)
+                }
+              >
+                <img
+                  style={ { width: '260px' } }
+                  alt="icone do botao"
+                  key={ index }
+                  src={ recipe.image }
+                  data-testid={ `${index}-horizontal-image` }
+                />
+              </button>
+
               <h3
                 data-testid={ `${index}-horizontal-top-text` }
               >
@@ -32,8 +42,14 @@ function DoneRecipes() {
 
               </h3>
 
-              <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
+              <button
+                onClick={ () => history.push(`/meals/${recipe.id}`) }
+              >
+                <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
+              </button>
+
               <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+
               <button
                 onClick={
                   () => clipboardCopy(`http://localhost:3000/meals/${recipe.id}`).then(() => {
@@ -63,14 +79,27 @@ function DoneRecipes() {
             </>)
           : (
             <>
-              <img
-                alt="icone do botao"
-                style={ { width: '260px' } }
-                key={ index }
-                src={ recipe.image }
-                data-testid={ `${index}-horizontal-image` }
-              />
-              <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
+              <button
+                type="button"
+                onClick={
+                  () => history.push(`/drinks/${recipe.id}`)
+                }
+              >
+                <img
+                  alt="icone do botao"
+                  style={ { width: '260px' } }
+                  key={ index }
+                  src={ recipe.image }
+                  data-testid={ `${index}-horizontal-image` }
+                />
+              </button>
+
+              <button
+                onClick={ () => history.push(`/drinks/${recipe.id}`) }
+              >
+                <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
+              </button>
+
               <h3
                 data-testid={ `${index}-horizontal-top-text` }
               >
